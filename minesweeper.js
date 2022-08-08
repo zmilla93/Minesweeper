217992a6474bc6d103098de901c78b7b0d56d57f
Zach Miller <zmilla93@gmail.com>
2022-08-08 14:01:26 -0500

    added border

--- minesweeper.js
+++ minesweeper.js
@@ -5,38 +5,54 @@ var canvas = document.getElementById("myCanvas");
 canvas.onmousedown = handleMouseDown;
 canvas.onmouseup = handleMouseUp;
 canvas.onmousemove = handleMouseMove;
+canvas.oncontextmenu = function (e) { e.preventDefault(); };
 // Get a reference to a 2D context of the canvas.
 // This is what holds the drawing functions.
 var ctx = canvas.getContext('2d');
 
 // Contants
-const iconSize = 16;
+const tileSize = 16;
+const borderSize = 10;
 const boardOffsetX = 10;
-const boardOffsetY = 10;
-const boarderSize = 10;
+const boardOffsetY = borderSize * 2 + tileSize * 2;
 
 // Images
 var imagesLoaded = 0;
-const imageCount = 4;
+const imageCount = 7;
 var facesImage = new Image();
 var numberImage = new Image();
 var numberDebugImage = new Image();
+var borderHorizontalImage = new Image();
+var borderVerticalImage = new Image();
+var cornersImage = new Image();
 var tileImage = new Image();
-var debugBombs = true;
-var debugNumbers = true;
+
+var faceSprites;
+var numberSprites;
+var tileSprites;
+var borderHorizontalSprite;
+var borderVerticalSprite;
+var cornerSprites;
+
+
+
 
 // Settings
 var tileCountX = 30;
 var tileCountY = 20;
 var bombCount = 10;
+var debugBombs = true;
+var debugNumbers = true;
 
 // Game
 var canvasRect;
 var boardRect;
-// var field;
 var tiles;
-// var mineField;
 var randomIndices;
+var ended = false;
+var endBomb;
+var targetRevealCount;
+var tilesRevealed;
 
 // Interactions
 var pressed = false;
@@ -46,17 +62,21 @@ var clickPos;
 // Bind UI
 window.onload = function () {
     var startButton = document.getElementById("startButton");
+    var beginnerButton = document.getElementById("beginnerButton");
+    var intermediateButton = document.getElementById("intermediateButton");
+    var advancedButton = document.getElementById("advancedButton");
     startButton.onclick = function () { startGame(30, 20, 1); }
+    beginnerButton.onclick = function () { startGame(9, 9, 10); }
+    intermediateButton.onclick = function () { startGame(16, 16, 30); }
+    advancedButton.onclick = function () { startGame(30, 16, 99); }
 };
 
 
 function handleMouseDown(e) {
     let x = e.x - canvasRect.x;
     let y = e.y - canvasRect.y;
-
-
     // if (x >= boardRect.x && y >= boardRect.y && x < boardRect.x + boardRect.width && y < boardRect.y + boardRect.height) {
-    if (isWithinBoard(x, y)) {
+    if (!ended && isWithinBoard(x, y)) {
         var pos = posToCoords(x, y);
         clickPos = posToCoords(x, y);
         drawTile(pos.x, pos.y, true);
@@ -69,10 +89,10 @@ function handleMouseDown(e) {
 function handleMouseUp(e) {
     let x = e.x - canvasRect.x;
     let y = e.y - canvasRect.y;
-    if (pressed && isWithinBoard(x, y)) {
-        // REVEAL TILE
+    if (!ended && pressed && isWithinBoard(x, y)) {
         var pos = posToCoords(x, y);
         revealTile(pos.x, pos.y);
+        checkWin();
         drawBoard();
     }
     pressed = false;
@@ -103,8 +123,8 @@ function isWithinBoard(x, y) {
 function posToCoords(x, y) {
     x -= boardOffsetX;
     y -= boardOffsetY;
-    x = Math.floor(x / iconSize);
-    y = Math.floor(y / iconSize);
+    x = Math.floor(x / tileSize);
+    y = Math.floor(y / tileSize);
     return { x: x, y: y }
 }
 
@@ -139,6 +159,9 @@ function loadSpriteSheets() {
     loadSpriteSheet(numberImage, "sprites/numbers.png");
     loadSpriteSheet(numberDebugImage, "sprites/numbers_debug.png");
     loadSpriteSheet(tileImage, "sprites/tiles.png");
+    loadSpriteSheet(borderHorizontalImage, "sprites/border_horizontal.png");
+    loadSpriteSheet(borderVerticalImage, "sprites/border_vertical.png");
+    loadSpriteSheet(cornersImage, "sprites/corners.png");
 }
 
 function drawTile(x, y, pressed) {
@@ -146,15 +169,29 @@ function drawTile(x, y, pressed) {
     index = 0;
     let tile = tiles[x][y];
     if (tile.revealed) {
-        image = numberImage;
-        index = tile.number;
+        if (tile.bomb) {
+            image = tileImage;
+            index = 5;
+            if (endBomb != null && endBomb.x == x && endBomb.y == y) index = 6;
+
+            // FIXME : Add red bomb
+        } else {
+            image = numberImage;
+            index = tile.number;
+        }
     } else {
         if (pressed) {
             index = 1;
         } else {
             if (debugNumbers) {
-                index = tile.number;
-                image = numberDebugImage;
+                if (tile.bomb) {
+                    image = tileImage;
+                    index = 8;
+
+                } else {
+                    image = numberDebugImage;
+                    index = tile.number;
+                }
             } else {
                 index = 0;
             }
@@ -165,61 +202,73 @@ function drawTile(x, y, pressed) {
 
 function drawTileFromSheet(sheet, index, x, y) {
     ctx.drawImage(sheet,
-        (iconSize + 1) * index, 0, iconSize, iconSize,      // Sprite Sheet Position
-        boardOffsetX + x * iconSize, boardOffsetY + y * iconSize, iconSize, iconSize);    // Canvas Position
+        (tileSize + 1) * index, 0, tileSize, tileSize,      // Sprite Sheet Position
+        boardOffsetX + x * tileSize, boardOffsetY + y * tileSize, tileSize, tileSize);    // Canvas Position
+}
+
+function drawBorders() {
+    ctx.fillStyle = '#C0C0C0';
+    ctx.fillRect(borderSize, borderSize, boardRect.width, tileSize*2)
+    // Corners Top
+    ctx.drawImage(cornersImage, 0, 0, borderSize, borderSize, 0, 0, borderSize, borderSize);
+    ctx.drawImage(cornersImage, borderSize * 1, 0, borderSize, borderSize, borderSize + boardRect.width, 0, borderSize, borderSize);
+    // Corners Mid
+    ctx.drawImage(cornersImage, borderSize * 2, 0, borderSize, borderSize, 0, borderSize + tileSize * 2, borderSize, borderSize);
+    ctx.drawImage(cornersImage, borderSize * 3, 0, borderSize, borderSize, borderSize + boardRect.width,  borderSize + tileSize * 2, borderSize, borderSize);
+    // Corners Bottom
+    ctx.drawImage(cornersImage, borderSize * 4, 0, borderSize, borderSize, 0, boardRect.y + boardRect.height, borderSize, borderSize);
+    ctx.drawImage(cornersImage, borderSize * 5, 0, borderSize, borderSize, borderSize + boardRect.width, boardRect.y + boardRect.height, borderSize, borderSize);
+
+    // Horizontal Bars
+    for (var i = 0; i < tileCountX; i++) {
+        ctx.drawImage(borderHorizontalImage, borderSize + tileSize * i, 0);
+        ctx.drawImage(borderHorizontalImage, borderSize + tileSize * i, borderSize + tileSize * 2)
+        ctx.drawImage(borderHorizontalImage, borderSize + tileSize * i, boardRect.y + boardRect.height)
+    }
+    // Vertical Bars
+    let offset = borderSize;
+    for (var i = 0; i < 2; i++) {
+        ctx.drawImage(borderVerticalImage, 0, offset + tileSize * i)
+        ctx.drawImage(borderVerticalImage, boardRect.x + boardRect.width, offset + tileSize * i)
+    }
+    offset = borderSize * 2 + tileSize * 2;
+    for (var i = 0; i < tileCountY; i++) {
+        ctx.drawImage(borderVerticalImage, 0, offset + tileSize * i)
+        ctx.drawImage(borderVerticalImage, boardRect.x + boardRect.width, offset + tileSize * i)
+    }
 }
 
 function drawBoard() {
     for (let x = 0; x < tileCountX; x++) {
         for (let y = 0; y < tileCountY; y++) {
             drawTile(x, y, false);
-            if (tiles[x][y].bomb) {
-                // drawTileFromSheet(tileImage, 8, x, y);
-                // ctx.drawImage(facesImage,
-                //     iconSize + 1, 0, iconSize, iconSize,
-                //     x * iconSize, y * iconSize, iconSize, iconSize);
-            } else {
-                // drawTileFromSheet(numberDebugImage, tiles[x][y].number, x, y)
-                // drawTile(tileImage, 2, x, y)
-                // ctx.drawImage(tileImage,
-                //     0, 0, iconSize, iconSize,
-                //     x * iconSize, y * iconSize, iconSize, iconSize);
-            }
         }
     }
+}
+
+function drawFace(){
+    ctx.drawImage(facesImage, 0, offset + tileSize * i)
 }
 
 function revealTile(x, y) {
     var tile = tiles[x][y];
     if (tile.revealed) return;
     tile.revealed = true;
-    // var neighborOffsets = [{ x: -1, y: 0 }, { x: 1, y: 0 }, { x: 0, y: -1 }, { x: 0, y: 1 },
-    // { x: -1, y: -1 }, { x: 1, y: -1 }, { x: -1, y: 1 }, { x: 1, y: 1 }];
-    // if (x == 28 && y == 19) {
-    //     console.log("Starting target tile...");
-    // }
+    tilesRevealed++;
     if (tile.number == 0) {
         for (var offsetX = -1; offsetX <= 1; offsetX++) {
             for (var offsetY = -1; offsetY <= 1; offsetY++) {
-                // if (x == 28 && y == 19) {
-                //     console.log("neighbor check...");
-                // }
                 if (offsetX == 0 && offsetY == 0) continue;
                 var neighborX = x + offsetX;
                 var neighborY = y + offsetY;
-                // if (x == 28 && y == 19) {
-                //     console.log("Neighbor: " + neighborX + ", " + neighborY);
-                // }
                 if (neighborX < 0 || neighborX >= tileCountX || neighborY < 0 || neighborY >= tileCountY) continue;
                 revealTile(neighborX, neighborY);
             }
         }
-        // for (let i = 0; i < neighborOffsets.length; i++) {
-        //     var neighborX = x + neighborOffsets[i].x;
-        //     var neighborY = y + neighborOffsets[i].y;
-        //     if (neighborX < 0 || neighborX >= tileCountX || neighborY < 0 || neighborY >= tileCountY) continue;
-        //     revealTile(neighborX, neighborY);
-        // }
+    } else if (tile.bomb) {
+        ended = true;
+        endBomb = { x: x, y: y };
+        endGame(false);
     }
 }
 
@@ -287,13 +336,18 @@ function generateNumbers(width, height) {
 }
 
 function startGame(width, height, bombs) {
-    canvasRect = canvas.getBoundingClientRect();
-    boardRect = { x: boardOffsetX, y: boardOffsetY, width: tileCountX * iconSize, height: tileCountY * iconSize }
-    canvas.width = boardOffsetX + boarderSize + tileCountX * iconSize;
-    canvas.height = boardOffsetY + boarderSize + tileCountY * iconSize;
+    ended = false;
+    endBomb = null;
+
     tileCountX = width;
     tileCountY = height;
+    boardRect = { x: boardOffsetX, y: boardOffsetY, width: tileCountX * tileSize, height: tileCountY * tileSize }
+    canvas.width = boardOffsetX + borderSize + tileCountX * tileSize;
+    canvas.height = boardOffsetY + borderSize + tileCountY * tileSize;
+    canvasRect = canvas.getBoundingClientRect();
     bombCount = bombs;
+    targetRevealCount = width * height - bombs;
+    tilesRevealed = 0;
     tiles = [width];
     randomIndices = [width * height]
 
@@ -301,7 +355,7 @@ function startGame(width, height, bombs) {
     for (let x = 0; x < width; x++) {
         tiles[x] = [height]
         for (let y = 0; y < height; y++) {
-            tiles[x][y] = { number: 0, bomb: false, revealed: false };
+            tiles[x][y] = { number: 0, bomb: false, revealed: false, flagged: false };
             randomIndices[toIndex(x, y)] = toIndex(x, y);
         }
     }
@@ -311,6 +365,28 @@ function startGame(width, height, bombs) {
     placeBombs(width, height);
     generateNumbers(width, height);
 
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBorders();
+    drawBoard();
+}
+
+function checkWin() {
+    if (targetRevealCount == tilesRevealed) {
+        endGame(true);
+    }
+}
+
+function endGame(win) {
+    ended = true;
+    for (x = 0; x < tileCountX; x++) {
+        for (y = 0; y < tileCountY; y++) {
+            let tile = tiles[x][y];
+            if (tile.bomb) {
+                tile.revealed = true;
+                if (win) tile.flagged = true;
+            }
+        }
+    }
     drawBoard();
 }
 
